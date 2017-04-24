5ee253c053a9b14283e5d6a7e3c09c85a527ba89
木下貴司 <takataka@TAKAnoMacBook-Pro.local>
2017-04-24 14:28:18 +0900

    config smtp

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,17 +1,16 @@
-config.action_mailer.default_url_options = { host:=> 'https://ancient-wildwood-62452.herokuapp.com/' }
-ActionMailer::Base.delivery_method = :smtp
-ActionMailer::Base.smtp_settings =
-{
- user_name: ENV['SENDGRID_USERNAME'],
- password: ENV['SENDGRID_PASSWORD'],
- address: "smtp.sendgrid.net",
- port: 587,
- authentication: :plain,
- enable_starttls_auto: true
-}
-
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
+  config.action_mailer.default_url_options = {host:'https://ancient-wildwood-62452.herokuapp.com/'}
+  ActionMailer::Base.delivery_method = :smtp
+  ActionMailer::Base.smtp_settings =
+  {
+   user_name: ENV['SENDGRID_USERNAME'],
+   password: ENV['SENDGRID_PASSWORD'],
+   address: "smtp.sendgrid.net",
+   port: 587,
+   authentication: :plain,
+   enable_starttls_auto: true
+  }
 
   # Code is not reloaded between requests.
   config.cache_classes = true
